79f11a7658ed8861a531e52c7e673b1406e8c180
idelc074 <elidezignacio@gmail.com>
2022-11-26 20:29:36 -0500

    Update

--- Prototype/Assets/Scripts/MenuScripts/PauseMenu.cs
+++ Prototype/Assets/Scripts/MenuScripts/PauseMenu.cs
@@ -12,7 +12,7 @@ public class PauseMenu : MonoBehaviour
 
     CursorLockMode desiredMode;
 
-
+    
     private void Update()
     {
         if(Input.GetKeyDown(KeyCode.Escape))
@@ -51,6 +51,6 @@ public class PauseMenu : MonoBehaviour
         isPaused = false;
 
         Cursor.visible = false;
-        desiredMode = CursorLockMode.Confined;
+        desiredMode = CursorLockMode.Locked;
     }
 }
